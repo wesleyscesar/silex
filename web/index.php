6012fb75bc1b8727b0d77a5fb5cce2fcdf626d4f
Wesley Cesar <wesley@criasoft.com.br>
2016-04-26 13:23:26 -0300

    - Projeto 3

--- web/index.php
+++ web/index.php
@@ -8,6 +8,25 @@ use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$app['array'] = function(){
+     $data = array(
+        0 => array("id" => 1, "conteudo" => "Conteudo1"),
+        1 => array("id" => 2, "conteudo" => "Conteudo2"),
+        2 => array("id" => 3, "conteudo" => "Conteudo3"),
+        3 => array("id" => 4, "conteudo" => "Conteudo4"),
+        4 => array("id" => 5, "conteudo" => "Conteudo5"),
+        5 => array("id" => 6, "conteudo" => "Conteudo6"),
+        6 => array("id" => 7, "conteudo" => "Conteudo7"),
+        7 => array("id" => 8, "conteudo" => "Conteudo8"),
+        8 => array("id" => 9, "conteudo" => "Conteudo9"),
+        9 => array("id" => 10, "conteudo" => "Conteudo10")
+    );
+
+    return $data;
+};
+
+$data = $app['array'];
+
 $app->mount("/", include '../web/src/Wesley/Controllers/post.php');
 
 $app->run();
--- web/src/Wesley/Controllers/post.php
+++ web/src/Wesley/Controllers/post.php
@@ -5,19 +5,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 $post = $app['controllers_factory'];
 
-$data = array(
-    0 => array("id" => 1, "conteudo" => "Conteudo1"),
-    1 => array("id" => 2, "conteudo" => "Conteudo2"),
-    2 => array("id" => 3, "conteudo" => "Conteudo3"),
-    3 => array("id" => 4, "conteudo" => "Conteudo4"),
-    4 => array("id" => 5, "conteudo" => "Conteudo5"),
-    5 => array("id" => 6, "conteudo" => "Conteudo6"),
-    6 => array("id" => 7, "conteudo" => "Conteudo7"),
-    7 => array("id" => 8, "conteudo" => "Conteudo8"),
-    8 => array("id" => 9, "conteudo" => "Conteudo9"),
-    9 => array("id" => 10, "conteudo" => "Conteudo10")
-);
-
 $post->get('/', function() use ($data) {
     $posts = "";
     foreach($data as $item){
